ab3d2398d00d926c26d5583756001cdafcd22ae4
stdweird <stijn.deweirdt@ugent.be>
2013-05-06 23:13:42 +0200

    convert unicode to str, as used in dict lookup

--- src/graphtool/web/__init__.py
+++ src/graphtool/web/__init__.py
@@ -3,27 +3,27 @@ import os
 import mimetools
 import mimetypes
 mimetypes.init()
-mimetypes.types_map['.dwg']='image/x-dwg'
-mimetypes.types_map['.ico']='image/x-icon'
+mimetypes.types_map['.dwg'] = 'image/x-dwg'
+mimetypes.types_map['.ico'] = 'image/x-icon'
 
 import cherrypy
 from graphtool.base.xml_config import XmlConfig
 from graphtool.base.iterator import ObjectIterator
 
-class WebHost( XmlConfig ):
- 
+class WebHost(XmlConfig):
+
   def __init__(self, *args, **kw):
       self.sub_objects = []
       super(WebHost, self).__init__(*args, **kw)
- 
-  def parse_dom( self ):
-    super( WebHost, self ).parse_dom( )
+
+  def parse_dom(self):
+    super(WebHost, self).parse_dom()
     self.sub_objects = []
     for mount in self.dom.getElementsByTagName('mount'):
-      self.parse_mount( mount )
+      self.parse_mount(mount)
     classes = self.globals
     for instance_dom in self.dom.getElementsByTagName('instance'):
-      if not ( instance_dom in self.dom.childNodes ):
+      if not (instance_dom in self.dom.childNodes):
         continue
       name = instance_dom.getAttribute("name")
       location = instance_dom.getAttribute("location")
@@ -34,15 +34,15 @@ class WebHost( XmlConfig ):
       instance = classes[ name ]
       instance.metadata['base_url'] = location
       if location != '':
-          cherrypy.tree.mount( instance, location )
+          cherrypy.tree.mount(instance, location)
       else:
-          cherrypy.tree.mount( instance, '/' + name )
+          cherrypy.tree.mount(instance, '/' + name)
       self.sub_objects.append(instance)
     for config in self.dom.getElementsByTagName('config'):
         text = config.firstChild
         if text and text.nodeType == text.TEXT_NODE:
             filename = str(text.data).strip()
-            filename =  self.expand_path( filename )
+            filename = self.expand_path(filename)
             module_name = config.getAttribute('module')
             if module_name != '':
                 try:
@@ -54,7 +54,7 @@ class WebHost( XmlConfig ):
                     filename)
             self.load_config(filename)
 
-  def parse_mount( self, mount_dom ):
+  def parse_mount(self, mount_dom):
     location = mount_dom.getAttribute('location')
     if (not location) or len(location) == 0:
       return
@@ -62,34 +62,34 @@ class WebHost( XmlConfig ):
     if not (content and len(content) > 0): content = None
     classes = self.find_classes()
     for class_dom in mount_dom.getElementsByTagName('class'):
-      instance = classes[class_dom.getAttribute('type')]( dom=class_dom )
+      instance = classes[class_dom.getAttribute('type')](dom=class_dom)
     for instance_dom in mount_dom.getElementsByTagName('instance'):
       instance = classes[instance_dom.getAttribute('name')]
-    self.mount_instance( instance, location, content )
+    self.mount_instance(instance, location, content)
     instance.metadata['base_url'] = location
 
-  def wrap_function(self, func, content=None, security_obj=None, authtype=None,\
-          access = None, timeout=None):
+  def wrap_function(self, func, content=None, security_obj=None, authtype=None, \
+          access=None, timeout=None):
       if security_obj == None:
-          def content_func( *args, **kw ):
+          def content_func(*args, **kw):
               if timeout:
-                  cherrypy.response.timeout=timeout
-              results = func( *args, **kw )
+                  cherrypy.response.timeout = timeout
+              results = func(*args, **kw)
               if content:
-                  cherrypy.response.headers['Content-Type'] = str(content) 
+                  cherrypy.response.headers['Content-Type'] = str(content)
               return results
           return content_func
       else:
           def content_func(*args, **kw):
               assert cherrypy.request.headers.get('SSL-CLIENT-VERIFY', \
                   'Failure') == 'SUCCESS'
-              dn = cherrypy.request.headers.get('SSL-CLIENT-S-DN',None)
+              dn = cherrypy.request.headers.get('SSL-CLIENT-S-DN', None)
               membership = kw.get(access, "Unknown")
               if not security_obj.authenticate(authtype, dn, membership):
                   cherrypy.response.headers["Status"] = 401
                   return "You are unauthorized to access this resource."
               if timeout:
-                  cherrypy.response.timeout=timeout
+                  cherrypy.response.timeout = timeout
               results = func(*args, **kw)
               if content:
                   cherrypy.response.headers['Content-Type'] = str(content)
@@ -97,7 +97,7 @@ class WebHost( XmlConfig ):
           return content_func
 
 
-  def mount_instance( self, instance, location, content=None ):
+  def mount_instance(self, instance, location, content=None):
     class DummyObject:
       _cp_config = {}
     do = DummyObject()
@@ -118,41 +118,41 @@ class WebHost( XmlConfig ):
                 security_authtype, security_access, timeout=timeout)
         else:
             func = self.wrap_function(func, content, timeout=timeout)
-        setattr( do, command, func )
+        setattr(do, command, func)
         func.__dict__['exposed'] = True
-        #print "Adding function %s as %s" % (func_name, command)
+        # print "Adding function %s as %s" % (func_name, command)
       except Exception, e:
         raise e
-    #print "Mounting instance %s at location %s" % (do, location)
-    cherrypy.tree.mount( do, location )
- 
-  def load_config( self, file ):
-    cherrypy.config.update( file )
+    # print "Mounting instance %s at location %s" % (do, location)
+    cherrypy.tree.mount(do, location)
+
+  def load_config(self, file):
+    cherrypy.config.update(file)
 
   def kill(self):
     for instance in self.sub_objects:
         if hasattr(instance, 'kill'):
             instance.kill()
- 
-class StaticContent( XmlConfig ):
 
-    _cp_config = {} 
-    _directory_paths={}
-    def index( self ):
+class StaticContent(XmlConfig):
+
+    _cp_config = {}
+    _directory_paths = {}
+    def index(self):
         return "No index here...!"
     index.exposed = True
 
     @cherrypy.expose
     def default(self, *args):
         try:
-            abspath=self._directory_paths[cherrypy.request.wsgi_environ['SCRIPT_NAME']]
+            abspath = self._directory_paths[cherrypy.request.wsgi_environ['SCRIPT_NAME']]
         except:
-            abspath="/"
-        file = os.path.join(abspath,*args)
+            abspath = "/"
+        file = os.path.join(abspath, *args)
         return cherrypy.lib.static.serve_file(file)
 
-    def parse_dom( self ):
-        super( StaticContent, self ).parse_dom()
+    def parse_dom(self):
+        super(StaticContent, self).parse_dom()
         for module_dom in self.dom.getElementsByTagName("module"):
             name = module_dom.getAttribute("name")
             mod_name_dom = module_dom.firstChild
@@ -165,7 +165,7 @@ class StaticContent( XmlConfig ):
             getattr(self, name).exposed = True
         for directory_dom in self.dom.getElementsByTagName('directory'):
             name = directory_dom.getAttribute('name')
-            location = directory_dom.getAttribute('location')
+            location = str(directory_dom.getAttribute('location'))
             if name == '':
                 continue
             directory_name_dom = directory_dom.firstChild
@@ -174,7 +174,7 @@ class StaticContent( XmlConfig ):
                 continue
             dir_name = str(directory_name_dom.data).strip()
             dir_name = self.expand_path(dir_name)
-            self._directory_paths[location]=dir_name
+            self._directory_paths[location] = dir_name
 
 
 class StaticModule(object):
@@ -185,8 +185,8 @@ class StaticModule(object):
         self.module = module
         self.rs = __import__("pkg_resources").resource_stream
         self.mimetypes = __import__("mimetypes")
-        self.mimetypes.types_map['.dwg']='image/x-dwg'
-        self.mimetypes.types_map['.ico']='image/x-icon'
+        self.mimetypes.types_map['.dwg'] = 'image/x-dwg'
+        self.mimetypes.types_map['.ico'] = 'image/x-icon'
 
     def __call__(self, path):
         # Snippet from CherryPy
@@ -204,10 +204,10 @@ class HelloWorld(object):
   static = cherrypy.tools.staticdir.handler(section='static', root=os.getcwd(),
                                      dir='static_content')
 
-  def __init__( self, *args, **kw ): pass
+  def __init__(self, *args, **kw): pass
 
-  def condor( self, *args, **kw ):
-    r = os.popen( 'condor_q -xml' )
+  def condor(self, *args, **kw):
+    r = os.popen('condor_q -xml')
     lines = r.readlines()
     xml_string = ''
     found_header = False
@@ -218,7 +218,7 @@ class HelloWorld(object):
       if found_header:
         if line.startswith('<!DOCTYPE'):
           continue
-        xml_string += line 
+        xml_string += line
         if line.startswith('<?xml version'):
           xml_string += '<?xml-stylesheet type="text/xsl" href="/static/content/condor_results.xsl"?>\n'
     cherrypy.response.headers['Content-Type'] = 'text/xml'
